b36260d25d0b1d48fe18d9657401d1a2c196822b
Belle Sugi Zafimarovololona <sugi.zaf@onja.org>
2021-01-20 10:31:34 +0300

    Footer

---
+++ src/components/footer/styles/footer.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import styled from 'styled-components'
+
+import Styled from 'styled-components'
+
+export const Container = styled.div`
+display: flex;
+`
+
+export const Column = styled.div`
+  
+`;
+export const Row  = styled.div`
+  
+`;
+export const Link = styled.a`
+  
+`;
+export const Title = styled.p`
+  
+`;
+export const Text = styled.p`
+  
+`;
+
+export const Break = styled.div`
+  
+`;
---
+++ src/pages/home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import JumbontronContainer from "../containers/jumbotron";
+import FooterContainer from '../containers/footer'
+
+export default function Home() {
+  return (
+    <>
+    <JumbontronContainer ></JumbontronContainer>
+    <FooterContainer></FooterContainer>
+    </>
+  );
+}
